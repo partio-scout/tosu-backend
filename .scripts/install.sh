421e6d36c1094876d2dc77cdaa90520c7770d50d
Kari Vänttinen <kari.vanttinen@helsinki.fi>
2018-01-26 13:46:31 +0200

    pipeline test again
    Former-commit-id: 531a9943fa1371c71b941322d28279d13ef9d0c4

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-openssl aes-256-cbc -K $encrypted_0521f70a21ab_key -iv $encrypted_0521f70a21$ -in deploy.pem.enc -out deploy.pem -d
+openssl aes-256-cbc -K $encrypted_0521f70a21ab_key -iv $encrypted_0521f70a21ab_iv -in deploy.pem.enc -out deploy.pem -d
 rm deploy.pem.enc
 chmod 600 deploy.pem
 mv deploy.pem ~/.travis/id_rsa
