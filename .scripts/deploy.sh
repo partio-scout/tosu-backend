18fcb58abff1757ef557db94c1a346fdb5ba5343
Kari Vänttinen <kari.vanttinen@helsinki.fi>
2018-01-26 15:24:58 +0200

    pipeline final try

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -4,11 +4,19 @@ eval "$(ssh-agent -s)"
 chmod 600 ~/.travis/id_rsa
 ssh-add ~/.travis/id_rsa
 
-git config --global push.default matching
-git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
-git push deploy
+#git config --global push.default matching
+#git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
+#git push deploy master
 
 ssh deploy@$IP -p $PORT <<EOF
     cd $DEPLOY_DIR
-    echo "jeejee" > testi.txt
+    cd ..
+    git clone https://github.com/partio-scout/tosu-backend.git
+    cd tosu-backend
+    gradle bootRepackage
+    mv build/libs/gs-rest-service-0.1.0.jar /var/www/html/backend.jar
+    cd /var/www/html
+    java -jar backend.jar &
+    cd /var/www
+    rm -rf tosu-backend
 EOF
