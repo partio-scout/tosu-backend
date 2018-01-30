56304e889c512761235015f8bc9b70e3b0e723b1
Minka Tahvanainen <minka.tahvanainen@gmail.com>
2018-01-30 18:45:17 +0200

    LessThan changed to After
    Former-commit-id: 1cb4f1d76cf89245f79c9bc8a265ddbee5fadd6e

--- src/main/java/partio/controller/EventController.java
+++ src/main/java/partio/controller/EventController.java
@@ -20,7 +20,7 @@ public class EventController {
     public void construct() {
         eventService.add("mennyt tapahtuma", LocalDateTime.of(1990, 5, 13, 3, 0), LocalDateTime.of(1990, 5, 14, 3, 0), "Tämän ei pitäisi näkyä listalla");
         eventService.add("Alkanut tapahtuma", LocalDateTime.of(2018, 1, 13, 3, 0), LocalDateTime.of(2018, 5, 14, 3, 0), "Tämän pitäisi näkyä listalla");
-        eventService.add("Tuleva tapahtuma", LocalDateTime.of(2018, 5, 13, 3, 0), LocalDateTime.of(2018, 5, 14, 3, 0), "Tämän pitäisi näkyä listalla");
+        eventService.add("Tuleva tapahtuma", LocalDateTime.of(2019, 5, 13, 3, 0), LocalDateTime.of(2019, 5, 14, 3, 0), "Tämän pitäisi näkyä listalla");
     }
 
     @GetMapping("/comingEvents")
--- src/main/java/partio/repository/EventRepository.java
+++ src/main/java/partio/repository/EventRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import partio.domain.Event;
 
 public interface EventRepository extends JpaRepository<Event, Long>{
-     List<Event> findByEndTimeLessThan(LocalDateTime date);
+     List<Event> findByEndTimeAfter(LocalDateTime date);
 
 }
