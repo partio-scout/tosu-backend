2061e3ac2747dacd1e5f1a397f127db05867e082
kapistelijaKrisu <krisuppi@yahoo.com>
2018-01-30 12:02:50 +0200

    runjava.sh updated to launch production profile. Procfile added to root. Configuration files updated in partio package.

--- src/main/java/partio/TestConfiguration.java
+++ src/main/java/partio/TestConfiguration.java
@@ -7,10 +7,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Profile("dev")
+@Profile("test")
 @Configuration
-public class DevelopmentConfiguration {
+public class TestConfiguration {
+    /*
+    Add here if some sort of test data is needed for exploratory testing.
+    This should not reach production. HOWEVER this needs to be confirmed.
     
+    Dev profile cannot exist separately. It has to come from spring itself.
+    Otherwise you will have to manually tell database address for example.
+    */
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource dataSource() {
