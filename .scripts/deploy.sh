cfa5dc12b026538a17dd17c0e4c32169d5805f64
Kari Vänttinen <kari.vanttinen@helsinki.fi>
2018-02-01 14:09:25 +0200

    fixing scripts
    Former-commit-id: a184b017b2b67a8bf083fc6b0f31e49da93b14ec

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -9,6 +9,8 @@ ssh-add ~/.travis/id_rsa
 #git push deploy master
 
 ssh deploy@$IP -p $PORT <<EOF
+    eval "$(ssh-agent -s)"
+    ssh-add ~/.ssh/deploy.pem
     cd /var/www/tosu-backend
     git pull
     gradle clean
