a91b70c3647bb7126f9428afc67ad76e7e864448
Kari Vänttinen <kvanttinen@gmail.com>
2018-02-07 14:43:18 +0200

    added class for hibernate to create tables
    Former-commit-id: 6971a385c70b1336f6782ee5648d0167b1b9fc5c

---
+++ src/main/java/partio/PostgreSQLDatabaseGenerator.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package partio;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.boot.spi.MetadataImplementor;
+import org.hibernate.tool.hbm2ddl.SchemaExport;
+import partio.domain.Event;
+
+/**
+ *
+ * @author kari
+ */
+public class PostgreSQLDatabaseGenerator {
+    
+    public void generate() {
+        Map<String, String> settings = new HashMap<>();
+        settings.put("spring.datasource.driverClassName", "org.postgresql.Driver");
+        settings.put("spring.jpa.database-platform", "org.hibernate.dialect.PostgreSQLDialect");
+        settings.put("hibernate.connection.url", "jdbc:postgresql://localhost:5432/tosudb?useSSL=false");
+        settings.put("hibernate.connection.username", "postgres");
+        settings.put("hibernate.connection.password", "tosudb");
+        settings.put("hibernate.hbm2ddl.auto", "create");
+        settings.put("show_sql", "true");
+        settings.put("server.port", "3001");
+
+        MetadataSources metadata = new MetadataSources(
+                new StandardServiceRegistryBuilder()
+                        .applySettings(settings)
+                        .build());
+        metadata.addAnnotatedClass(Event.class);
+        SchemaExport schemaExport = new SchemaExport(
+                (MetadataImplementor) metadata.buildMetadata()
+        );
+        schemaExport.setHaltOnError(true);
+        schemaExport.setFormat(true);
+        schemaExport.setDelimiter(";");
+        schemaExport.setOutputFile("db-schema.sql");
+        schemaExport.execute(true, true, false, true);
+    }
+    
+}
