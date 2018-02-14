929261c52b15ec774cb2f6b766e8f09dc708ac6e
Kari Vänttinen <kvanttinen@gmail.com>
2018-02-14 13:46:01 +0200

    script update, run server scripts only for master branch

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -7,12 +7,13 @@ ssh-add ~/.travis/id_rsa
 #git config --global push.default matching
 #git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
 #git push deploy master
-
-ssh deploy@$IP -p $PORT <<EOF
-    cd /var/www/
-    git clone https://github.com/partio-scout/tosu-backend.git
-    cp certificate.p12 /var/www/tosu-backend/src/main/resources/certificate.p12
-    cp -f application-production.properties /var/www/tosu-backend/src/main/resources/application-production.properties
-    cd tosu-backend
-    gradle bootRepackage
-EOF
+if [[ $TRAVIS_BRANCH = 'master' ]]
+    ssh deploy@$IP -p $PORT <<EOF
+        cd /var/www/
+        git clone https://github.com/partio-scout/tosu-backend.git
+        cp certificate.p12 /var/www/tosu-backend/src/main/resources/certificate.p12
+        cp -f application-production.properties /var/www/tosu-backend/src/main/resources/application-production.properties
+        cd tosu-backend
+        gradle bootRepackage
+    EOF
+fi
