e2a1a9a1e3d72fef89c63433c4a4aa17c35942cb
kapistelijaKrisu <krisuppi@yahoo.com>
2018-03-02 18:15:34 +0200

    update

--- src/main/java/partio/WebConfig.java
+++ src/main/java/partio/WebConfig.java
@@ -2,13 +2,12 @@ package partio;
 
 /*
 import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
-*/
+ */
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -21,32 +20,14 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://localhost:3000", "http://localhost:3000")
+                .allowedOrigins("https://localhost:3000",
+                        "http://localhost:3000",
+                        "https://suunnittelu.partio-ohjelma.fi")
                 .allowedMethods("PUT", "DELETE", "POST", "GET");
-        
-        //jos haluaa tarkentaa näitä myöhemmin
-        //		.allowedHeaders("header1", "header2", "header3")
-        //		.exposedHeaders("header1", "header2")
-        //		.allowCredentials(false).maxAge(3600); //cache jtn
     }
 
-    //KARI KATO NAA ETTEI MEE SUN KONFFAUKSIEN KAA SEKAISIN
-    //corssi haluaa että määritellään onko http vai https
-    //->ennen pushia masteriin joutuis vaihtaa kaikki frontin http ->https
-    //tän takia tein nämäkin tähän näi kun google näytti nää samalla
-    
-    //laiton keytoolin ssl sisään resourcekansioon ja
-    //jos hyväksyt nää ota kommentit pois application.properties
-    //tein tän ssl:n frontti herjas ja tämä ehkä auttaa devaukses
-    
-    
-
-    //bonuksena jos http pyynto tulee se redirectaa sen
-    //en ole varma toimiiko frontin mukana kivasti
-    //huono puoli localhost aina haluaa portin localhost/event ei toimi
-    //redirectaa 8080->3001 ihan hyvin kuitenkin
-    
-    /*
+    /*  depending how front urls are solved we will decide if this is needed
+    it forces app to use ssl
     @Bean
     public EmbeddedServletContainerFactory servletContainer() {
         TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
@@ -60,19 +41,6 @@ public class WebConfig extends WebMvcConfigurerAdapter {
                 context.addConstraint(securityConstraint);
             }
         };
-
-    //tässä lisätään redirect portti eli alempi metodi
-        tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
         return tomcat;
-    }    
-    //new connctor redirect http reqs to https
-    private Connector initiateHttpConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setScheme("http");
-        connector.setPort(8080);
-        connector.setSecure(false);
-        connector.setRedirectPort(3001);
-
-        return connector;
     }*/
 }
