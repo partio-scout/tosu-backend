7402e193aadd924d5cf9be59511dbfc39d261489
kapistelijaKrisu <krisuppi@yahoo.com>
2018-03-03 15:47:29 +0200

    finished and cleaned

--- src/main/java/partio/WebConfig.java
+++ src/main/java/partio/WebConfig.java
@@ -1,13 +1,5 @@
 package partio;
 
-/*
-import org.apache.catalina.Context;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
-import org.springframework.context.annotation.Bean;
- */
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -25,22 +17,4 @@ public class WebConfig extends WebMvcConfigurerAdapter {
                         "https://suunnittelu.partio-ohjelma.fi")
                 .allowedMethods("PUT", "DELETE", "POST", "GET");
     }
-
-    /*  depending how front urls are solved we will decide if this is needed
-    it forces app to use ssl
-    @Bean
-    public EmbeddedServletContainerFactory servletContainer() {
-        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
-        return tomcat;
-    }*/
 }
