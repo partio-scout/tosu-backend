6929a0ed68084b0bb2ce86a9fe7ba317d7d7ab07
kapistelijaKrisu <krisuppi@yahoo.com>
2018-03-05 19:33:53 +0200

    revert reverting by fixing sort

--- src/main/java/partio/repository/EventRepository.java
+++ src/main/java/partio/repository/EventRepository.java
@@ -1,11 +1,16 @@
 package partio.repository;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import partio.domain.Event;
 import partio.domain.EventGroup;
 
+
 public interface EventRepository extends JpaRepository<Event, Long>{
     long countByGroupId(EventGroup group);
-     
-
+        
 }
+
