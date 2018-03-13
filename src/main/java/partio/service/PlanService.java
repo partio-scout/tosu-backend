4456f192beda49305bfccb6e1935ebe1c721caeb
Minka Tahvanainen <minka.tahvanainen@gmail.com>
2018-03-13 18:14:37 +0200

    Plan validation

--- src/main/java/partio/service/PlanService.java
+++ src/main/java/partio/service/PlanService.java
@@ -10,6 +10,7 @@ import partio.domain.Activity;
 import partio.domain.Plan;
 import partio.repository.ActivityRepository;
 import partio.repository.PlanRepository;
+import partio.service.validators.PlanValidator;
 
 @Service
 @Transactional
@@ -19,19 +20,38 @@ public class PlanService {
     private ActivityRepository activityRepository;
     @Autowired
     private PlanRepository planRepository;
+    @Autowired
+    private PlanValidator validator;
 
     public ResponseEntity<Object> removePlan(Long planId) {
         Plan toDelete = planRepository.findOne(planId);
         if (toDelete == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
-        
+
         planRepository.delete(toDelete);
         return ResponseEntity.ok(toDelete);
     }
 
     public ResponseEntity<Object> addPlan(Long activityId, Plan plan) {
         Activity activity = activityRepository.getOne(activityId);
+
+        if (activity == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+
+        List<String> errors = validator.validateNew(plan);
+
+        if (!errors.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+        }
+
+        for(Plan planExist : activity.getPlans()){
+            if(planExist.getGuid().equals(plan.getGuid())){
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Plan may only be added to activity once.");
+            }
+        }
+        
         activity.getPlans().add(plan);
 
         plan.setActivity(activity);
--- src/main/java/partio/service/validators/PlanValidator.java
+++ src/main/java/partio/service/validators/PlanValidator.java
@@ -11,6 +11,9 @@ import partio.repository.PlanRepository;
 @Service
 public class PlanValidator extends Validator<Plan> {
 
+    private static final int MAX_GUID_LENGTH = 127;
+    private static final int MIN_GUID_LENGTH = 1;
+
     @Autowired
     private ActivityRepository acitvityRepository;
 
@@ -20,11 +23,21 @@ public class PlanValidator extends Validator<Plan> {
     @Override
     public List<String> validateNew(Plan plan) {
         List<String> errors = validateNewAndOld(plan);
+        if (!validateStringLength(plan.getGuid(), MIN_GUID_LENGTH, MAX_GUID_LENGTH, NOT_NULL)) {
+            errors.add("guid length has to be between " + MIN_GUID_LENGTH + "-" + MAX_GUID_LENGTH);
+        }
+
+        if (!validateStringNotOnlySpaces(plan.getGuid(), NOT_NULL)) {
+            errors.add("guid cannot be whitespace only");
+        }
+        
         return errors;
     }
 
     @Override
     public List<String> validateChanges(Plan original, Plan changes) {
+        validateNew(changes);
+        validateNewAndOld(original);
         List<String> errors = new ArrayList<>();
         return errors;
     }
@@ -33,20 +46,13 @@ public class PlanValidator extends Validator<Plan> {
     protected List<String> validateNewAndOld(Plan plan) {
         List<String> errors = new ArrayList<>();
 
-        String url = plan.getUrl();
         if (plan.getActivity() != null) {
             errors.add("Execution plan can't exist without activity");
-        }
-        if (!isValid(plan.getUrl())) {
-            errors.add("Url isn't valid.");
         }
 
         return errors;
     }
 
-    private boolean isValid(String url) {
-        //coming soon
-        return true;
-    }
+
 
 }
