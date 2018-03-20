87e0ea61d38fb49d18b8e6b648089021f9775fdc
kapistelijaKrisu <krisuppi@yahoo.com>
2018-03-20 18:14:47 +0200

    plan 1st iteration done, check controller for request paths

--- src/main/java/partio/service/PlanService.java
+++ src/main/java/partio/service/PlanService.java
@@ -1,5 +1,6 @@
 package partio.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,6 +11,7 @@ import partio.domain.Activity;
 import partio.domain.Plan;
 import partio.repository.ActivityRepository;
 import partio.repository.PlanRepository;
+import partio.service.validators.PlanValidator;
 
 @Service
 @Transactional
@@ -19,6 +21,8 @@ public class PlanService {
     private ActivityRepository activityRepository;
     @Autowired
     private PlanRepository planRepository;
+    @Autowired
+    private PlanValidator validator;
 
     public ResponseEntity<Object> removePlan(Long planId) {
         Plan toDelete = planRepository.findOne(planId);
@@ -31,19 +35,50 @@ public class PlanService {
     }
 
     public ResponseEntity<Object> addPlan(Long activityId, Plan plan) {
-        Activity activity = activityRepository.getOne(activityId);
-        activity.getPlans().add(plan);
-
+        Activity activity = activityRepository.findOne(activityId);
+        if (activity == null) {    
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        
+        //set defaults if empty..ask customer more about it. If defaults are fine can remove validation later.
+        if (plan.getContent() == null || plan.getContent().trim().isEmpty()) {
+            plan.setContent("Tyhjä suunnitelma");
+        }
+        if (plan.getTitle() == null || plan.getTitle().trim().isEmpty()) {
+            plan.setTitle("Tyhjä otsikko");
+        }
         plan.setActivity(activity);
-
+        
+        List<String> errors = validator.validateNew(plan);
+        if (!errors.isEmpty()) {
+             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+        }
         planRepository.save(plan);
-        activityRepository.save(activity);
-
         return ResponseEntity.ok(plan);
     }
+    
+    public ResponseEntity<Object> modifyPlan(Plan plan, Long planId) {
+        if (plan == null) {    
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+        
+        Plan original = planRepository.findOne(planId);    
+        List<String> errors = validator.validateChanges(original, plan);
+        if (!errors.isEmpty()) {
+             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+        }
+        original.setContent(plan.getContent());
+        original.setTitle(plan.getTitle());
+        planRepository.save(original);
+        return ResponseEntity.ok(original);
+    }
 
-    public List<Plan> list(Long activityId) {
-        return activityRepository.getOne(activityId).getPlans();
+    public ResponseEntity<Object> list(Long activityId) {
+        Activity activity = activityRepository.findOne(activityId);
+        if (activity == null) {    
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return ResponseEntity.ok(activity.getPlans());
     }
 
 }
