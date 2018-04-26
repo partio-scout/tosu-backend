f2d04397330e9a3cae902f71561c832db348fdf2
Minka Tahvanainen <minka.tahvanainen@gmail.com>
2018-04-26 15:25:03 +0300

    Scout has buffer and buffer has scout

--- src/main/java/partio/domain/ActivityBuffer.java
+++ src/main/java/partio/domain/ActivityBuffer.java
@@ -1,4 +1,3 @@
-
 package partio.domain;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
@@ -7,11 +6,14 @@ import java.util.Objects;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.Proxy;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 import partio.jsonconfig.ActivityBufferSerializer;
 
@@ -22,10 +24,15 @@ import partio.jsonconfig.ActivityBufferSerializer;
 //@Proxy(lazy=false)
 @JsonSerialize(using = ActivityBufferSerializer.class)
 public class ActivityBuffer extends AbstractPersistable<Long> {
+
     public static final int BUFFER_SIZE = 10;
-   // @JsonManagedReference
+    // @JsonManagedReference
     @OneToMany(mappedBy = "buffer", cascade = CascadeType.REMOVE, orphanRemoval = true, fetch = FetchType.EAGER)
     private List<Activity> activities;
+    
+//    @OneToOne(mappedBy = "buffer")
+//    @Fetch(value = FetchMode.SUBSELECT)
+    private Scout scout;
 
     @Override
     public int hashCode() {
@@ -33,7 +40,7 @@ public class ActivityBuffer extends AbstractPersistable<Long> {
         hash = 37 * hash + Objects.hashCode(super.getId());
         return hash;
     }
-    
+
     public boolean equals(Object other) {
         if (other.getClass() != ActivityBuffer.class) {
             return false;
@@ -41,6 +48,5 @@ public class ActivityBuffer extends AbstractPersistable<Long> {
         ActivityBuffer otherGroup = (ActivityBuffer) other;
         return this.getId() == otherGroup.getId();
     }
-    
 
 }
