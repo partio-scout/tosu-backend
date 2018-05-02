d67a5000a9a1fddf16e07615eb291f717f00eb68
kapistelijaKrisu <krisuppi@yahoo.com>
2018-05-02 12:55:59 +0300

    try nr.5

--- src/main/java/partio/controller/OptionsController.java
+++ src/main/java/partio/controller/OptionsController.java
@@ -7,7 +7,6 @@ package partio.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,12 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author kbvalto
  */
-@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class OptionsController {
     //preflight response to axios, maybe fix?
      @RequestMapping(method = RequestMethod.OPTIONS)
     public ResponseEntity handle() {
         return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+    @RequestMapping(value = "/**", method = RequestMethod.OPTIONS)
+    public ResponseEntity handle2() {
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
